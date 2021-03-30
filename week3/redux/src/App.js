84c5fa6cc088b88a6f9474ce3157fca08ba678ed
michaelkosub/fsw-100 <michael.kosub@bryanuniversity.edu>
2021-03-30 10:07:58 -0500

    stopwatch

---
+++ week3/redux/src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Stopwatch</h1>
+        <Stopwatch status={false} runningTime={0} />
+      </div>
+    );
+  }
+}
+const formattedSeconds = (millis) => {
+  var hours = Math.floor(millis / 360000);
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  var milliseconds = (Math.floor(millis % 1000)).toFixed(0);
+  return hours + ":" + minutes + ":" + (seconds < 10 ? "0" : "") + seconds + ":" + milliseconds;
+};
+class Stopwatch extends Component {
+  state = {
+    status: false,
+    runningTime: 0,
+    laps: [],
+  };
+  handleChange = () => {
+    this.setState((state) => {
+      if (state.status) {
+        clearInterval(this.timer);
+      } else {
+        const startTimer = Date.now() - this.state.runningTime;
+        this.timer = setInterval(() => {
+          this.setState({ runningTime: Date.now() - startTimer });
+        });
+      }
+      return { status: !state.status };
+    });
+  };
+  handleReset = () => {
+    clearInterval(this.timer);
+    this.setState({ runningTime: 0, running: false });
+  };
+  handleLabClick() {
+    this.setState({
+      laps: this.state.laps.concat([this.state.runningTime]),
+    });
+  }
+  render() {
+    const { status, runningTime,  } = this.state;
+    return (
+      <div className="stopwatch">
+        <div className="stopwatch-timer">
+          <p>{formattedSeconds(runningTime)}</p>
+          <button onClick={this.handleChange}>
+            {status ? "Stop" : "Start"}
+          </button>
+          <button onClick={this.handleReset}>Reset</button>
+          <button onClick={this.handleLabClick.bind(this)}>Lap</button>
+        </div>
+        <ul className="stopwatch-laps">
+          {this.state.laps.map((lap, i) => (
+            <li className="stopwatch-lap">
+              <strong>{i + 1}</strong>
+              {formattedSeconds(lap)}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+export default App;
